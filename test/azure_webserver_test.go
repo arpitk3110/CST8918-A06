7a330eef0df91ba9d38652398bce87ad7a435e0c
Arpit Patel <pate1562@algonquinlive.com>
2026-03-12 20:44:48 -0400

    Add Terratest for Azure VM creation with automated test

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
@@ -10,11 +11,18 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../",
 		Vars: map[string]interface{}{
-			"labelPrefix": "arpit patel",
+			"labelPrefix":    "arpitpatel",
+			"admin_username": "azureadmin",
+			"region":         "eastus2",
 		},
+		MaxRetries:         3,
+		TimeBetweenRetries: 15 * time.Second,
 	}
 
-	defer terraform.Destroy(t, terraformOptions)
+	defer func() {
+		time.Sleep(60 * time.Second)
+		terraform.Destroy(t, terraformOptions)
+	}()
 
 	terraform.InitAndApply(t, terraformOptions)
 }
